4aee2451f8d34f7c40b9ed3d69c1822850652798
janar <janardhanp008@gmail.com>
2026-08-05 23:20:14 +0530

    Refactor tests: split MoreValidations into Frames, JS popups, mouse hover, hidden text tests

---
+++ tests/Java_JSPopups.spec.js
@@ -0,0 +1,11 @@
+await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+// await page.goto("http://google.com");
+// await page.goBack();
+// await page.goForward();
+await expect(page.locator("#displayed-text")).toBeVisible();
+await page.locator("#hide-textbox").click();
+await expect(page.locator("#displayed-text")).toBeHidden();
+await page.pause();
+page.on('dialog', dialog => dialog.accept());
+await page.locator("#confirmbtn").click();
---
+++ tests/MouseHover.spec.js
@@ -0,0 +1,8 @@
+await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+// await page.goto("http://google.com");
+// await page.goBack();
+// await page.goForward();
+
+await page.locator("#mousehover").click();
+
